756a443f580512484c6bf635b3871b8779f6a24e
JokyOFC <personal_joky@outlook.com>
2026-01-18 14:24:29 -0300

    feat: Adding Vue Bits to project

---
+++ front/portifolio-mise/src/components/Header.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+    import DecryptedText from './DecryptedText/DecryptedText.vue';
+</script>
+
+<template>
+
+    <DecryptedText
+        text="Hello World! Im trying to use Vue"
+        :speed="70"
+        :max-iterations="15"
+        :sequential="true"
+        reveal-direction="start"
+        :use-original-chars-only="false"
+        animate-on="hover"
+        class-name="text-green-500"
+        encrypted-class-name="text-red-500"
+    />
+
+</template>
